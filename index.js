dabfdebf3b5694fa5909f85edb9324dd208f45d7
Natalia Rodriguez <Natarodred@gmail.com>
2024-11-11 19:53:45 +0200

    initial push + hasta nodemon install

---
+++ index.js
@@ -0,0 +1,7 @@
+/*const generateRandomNumber = require('./utils');
+
+console.log(`Random Number: ${generateRandomNumber()}`);*/
+
+import getPost,{getPostLength}from './postController.js';
+console.log(getPost());
+console.log(getPostLength());
---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts =[
+    {id:1,tittle:'Post One'},
+    {id:2,tittle:'Post Two'}
+];
+
+const getPost = () => posts;
+
+export const getPostLength=()=>posts.length;
+
+export default getPost;
