b6c42bb98a4fca58eb2effb0a2328c3b4e29f763
Natalia Rodriguez Redondo <natalia.rodriguez.redondo@alumnos.upm.es>
2024-12-23 17:05:02 +0100

    middleware

--- express-crash/routes/posts.js
+++ express-crash/routes/posts.js
@@ -2,10 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 let posts=[
-    {id: 1, tittle: 'post One'},
-    {id: 2, tittle: 'post Two'},
-    {id: 3, tittle: 'post Three'},
+    {id: 1, title: 'post One'},
+    {id: 2, title: 'post Two'},
+    {id: 3, title: 'post Three'},
 ];
+
+
 
 //get all posts
 router.get('/',(req,res)=>{
@@ -42,8 +44,32 @@ router.post('/', (req,res)=>{
     if(!newPost.title){
         return res.status(400).json({msg:`Please include a Title`});
     }
-    posts.push(newPost);
+    posts.push(newPost);2
     res.status(201).json(posts);
+});
+
+//update post
+router.put('/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=>post.id===id);
+
+    if(!post){
+        return res.status(404).json({msg:`A post with the id of ${id} was not found`});
+    }
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
+//DELETE post
+router.delete('/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=>post.id===id);
+
+    if(!post){
+        return res.status(404).json({msg:`A post with the id of ${id} was not found`});
+    }
+    posts = posts.filter((post)=> post.id !== id);
+    res.status(200).json(posts);
 });
 
 export default router;
