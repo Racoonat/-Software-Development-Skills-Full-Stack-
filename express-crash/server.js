b3626a550117d782f480c50fed3fbe93628bf0e3
Natalia Rodriguez Redondo <natalia.rodriguez.redondo@alumnos.upm.es>
2024-12-23 15:37:11 +0100

    query strings

--- express-crash/server.js
+++ express-crash/server.js
@@ -1,10 +1,36 @@
 const express = require ('express');
 const path = require('path');
+const port = process.env.PORT || 8000;
 
 const app = express ();
 
 
 //setup static folder
-app.use(express.static(path.join(__dirname,'public')))
+//app.use(express.static(path.join(__dirname,'public')))
 
-app.listen(8000,()=> console.log(`Server is running on port 8000`));
+let posts=[
+    {id: 1, tittle: 'post One'},
+    {id: 2, tittle: 'post Two'},
+    {id: 3, tittle: 'post Three'},
+];
+
+
+//get all posts
+app.get('/api/posts',(req,res)=>{
+    console.log(req.query);
+    const limit = parseInt(req.query.limit);
+    if (!isNaN(limit)&&limit>0){
+        res.json(posts.slice(0,limit))
+    }else{
+    res.json(posts);
+    }
+});
+
+//get single posts
+app.get('/api/posts/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post)=>post.id === id));
+});
+
+
+app.listen(port,()=> console.log(`Server is running on port ${port}`));
