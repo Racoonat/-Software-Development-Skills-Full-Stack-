6ee784ded8fc9f26b636a8eb9cf0838f17d9b7b3
Natalia Rodriguez Redondo <natalia.rodriguez.redondo@alumnos.upm.es>
2024-12-23 16:03:21 +0100

    Post Request

--- express-crash/server.js
+++ express-crash/server.js
@@ -1,36 +1,19 @@
-const express = require ('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
 
 const app = express ();
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //setup static folder
 //app.use(express.static(path.join(__dirname,'public')))
 
-let posts=[
-    {id: 1, tittle: 'post One'},
-    {id: 2, tittle: 'post Two'},
-    {id: 3, tittle: 'post Three'},
-];
 
-
-//get all posts
-app.get('/api/posts',(req,res)=>{
-    console.log(req.query);
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit)&&limit>0){
-        res.json(posts.slice(0,limit))
-    }else{
-    res.json(posts);
-    }
-});
-
-//get single posts
-app.get('/api/posts/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    res.json(posts.filter((post)=>post.id === id));
-});
-
+//ROUTES
+app.use('/api/posts',posts);
 
 app.listen(port,()=> console.log(`Server is running on port ${port}`));
